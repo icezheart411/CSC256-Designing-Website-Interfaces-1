aaa42026d4694811d25abb9d6dbab8b1129f6956
icezheart411 <silentscreamz@comcast.net>
2026-07-11 20:23:14 -0400

    Add files via upload

---
+++ Week 1 Assignment 1.2 Review and Practice HTML-CSS-JavaScript/script.js
@@ -0,0 +1,9 @@
+// main JS code
+// setting the name id value
+document.getElementById("divName").textContent = "Mike Davis";
+// setting the major id value
+document.getElementById("divMajor").textContent = "Confused";
+// setting the email id value
+document.getElementById("divEmail").textContent = "mdavis97789@uat.edu";
+// setting the graduation date id value
+document.getElementById("divGradDate").textContent = "July 1, 2029";
